697aba9321e431c58e8a613829ca923b59ab1a19
plattyp <andrew.platkin@gmail.com>
2014-12-20 18:15:00 -0600

    fixed the destroy method in the images controller to just redirect to back

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,17 +3,9 @@ class ImagesController < ApplicationController
 	def destroy
 		@image = Image.find(params[:id])
 		if @image.destroy
-			if @image.imageable_type == "Post"
-				redirect_to edit_post_path(params[:class_id]), :notice => "The image was deleted successfully"
-			elsif @image.imageable_type == "Project"
-				redirect_to edit_project_path(params[:class_id]), :notice => "The image was deleted successfully"
-			end
+			redirect_to :back, :notice => "The image was deleted successfully"
 		else
-			if @image.imageable_type == "Post"
-				redirect_to edit_post_path(params[:class_id]), :notice => "The image was unable to be deleted"
-			elsif @image.imageable_type == "Project"
-				redirect_to edit_project_path(params[:class_id]), :notice => "The image was unable to be deleted"
-			end
+			redirect_to :back, :alert => "The image was not able to be deleted"
 		end
 	end
 end
