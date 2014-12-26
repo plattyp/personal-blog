58e6d926d884e060208bf706f98bf5da6280348f
plattyp <andrew.platkin@gmail.com>
2014-12-26 12:28:19 -0600

    updated comments in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 	end
 
 	def sendmessage
-		#Checks to see the message is not blank, if it isn't the message is sent
+		#Checks to see the message is not blank. if it isn't, the message is sent
 		unless params[:message].blank?
 			UserMessage.send_user_message(params[:user_id],params[:message]).deliver
 			redirect_to :back, :notice => "Your message was sent successfully!"
