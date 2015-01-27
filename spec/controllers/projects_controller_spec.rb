19a8800727ad01a947fddddd3229833724ad1ac7
plattyp <andrew.platkin@gmail.com>
2015-01-27 06:56:13 -0600

    started skeleton of Project specs

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -5,4 +5,29 @@ describe PostsController do
 		# Before filters on all methods
 		expect(controller).to receive(:get_metadata)
 	end
+
+	describe 'GET #index' do
+
+	end
+
+	describe 'GET #show' do
+	end
+
+	describe 'GET #new' do
+	end
+
+	describe 'POST #create' do
+	end
+
+	describe 'GET #edit' do
+	end
+
+	describe 'PATCH #update' do
+	end
+
+	describe 'DELETE #destroy' do
+	end
+
+	describe 'GET #manage' do
+	end
 end
