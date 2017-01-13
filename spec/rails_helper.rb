1d061ea5b2d7d0f6b142313acd7b301c1ae4247d
Andrew Platkin <andrew.platkin@gmail.com>
2017-01-12 21:38:55 -0600

    another attempt to fix travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,6 +42,7 @@ RSpec.configure do |config|
 
   config.before(:each) do
     allow_any_instance_of(Paperclip::Attachment).to receive(:save).and_return(true)
+    allow_any_instance_of(Paperclip::Attachment).to receive(:clear).and_return(true)
     allow_any_instance_of(Paperclip::Attachment).to receive(:destroy).and_return(true)
   end
 
